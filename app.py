33e6d75ea6f775c760a15d0155b6c9ef8f4be9da
Jad Isaac <jadjrisaac@gmail.com>
2025-09-16 15:43:38 -0700

    testing clone

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+import torch
 import sounddevice as sd
-import soundfile as sf
 from scipy.io.wavfile import write
+from TTS.api import TTS
+import soundfile as sf
 
 def record_voice(filename='my_voice.wav', duration=5, samplerate=16000) -> str:
     print('Recording Started...')
@@ -11,10 +15,30 @@ def record_voice(filename='my_voice.wav', duration=5, samplerate=16000) -> str:
     print(f'Saved voice recording as {filename}')
     return filename
 
-def clone_voice(text: str, reference_audio_path: str, output_dir="cloned_output") -> str:
-    return ""
+def clone_voice(text: str, reference_audio_path: str, output_dir="cloned_outputs") -> str:
+    # initilizing model tts
+    tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=True)
+
+    # make directory which will save cloned voice
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_path = os.path.join(output_dir, f"cloned_{uuid.uuid4().hex[:8]}.wav")
+
+    cloned_voice = tts.tts_with_vc(
+        text = text,
+        speaker_wav = reference_audio_path,
+        speaker = 'random',
+        language="en"
+    )
+
+    # saving cloned voice
+    sf.write(output_path, cloned_voice, tts.synthesizer.output_sample_rate)
+    print('saved cloned voice')
+    return output_path
 
 if __name__ == "__main__":
-    refrence_path = record_voice()
+    reference_path = record_voice()
 
     text_to_speak = "Hello World! This is your cloned voice using pytorch and yourtts"
+
+    output_path = clone_voice(text_to_speak, reference_path)
